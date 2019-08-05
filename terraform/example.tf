679042b290cf276b5fecd348ba7b8918f23c09a6
DimitryRd <dimitry.rd@gmail.com>
2019-08-06 02:23:22 +0300

    Add IAM policy, role and user

--- terraform/example.tf
+++ terraform/example.tf
@@ -3,12 +3,39 @@ provider "aws" {
   region     = "${var.region}"
 }
 
+data "aws_iam_user" "rudakov" {
+  user_name = "rudakov"
+}
+
 data "aws_iam_policy_document" "example" {
+
   statement {
     actions   = ["*"]
     resources = ["*"]
   }
 }
+
+resource "aws_iam_role" "ec2_iam_role" {
+  name = "ec2_iam_role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": [
+          "ec2.amazonaws.com"
+        ]
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
 
 resource "aws_iam_policy" "example" {
   # ... other configuration ...
